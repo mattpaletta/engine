1617adf4a45ccd49f530213790be11e4dc090c5c
Matthew Paletta <mattpaletta@gmail.com>
2020-06-01 14:12:05 -0700

    Added optional linking of filesystem library for gcc < 9.0

--- external/openvr.cmake
+++ external/openvr.cmake
@@ -1,5 +1,5 @@
 # Fetch OpenVR
 set(OPENVR_VERSION v1.11.11)
 set(BUILD_SHARED ON)
-set(USE_LIBCXX OFF)
+option(USE_LIBCXX "use libcxx for openvr" OFF)
 fetch_extern(openvr https://github.com/ValveSoftware/openvr ${OPENVR_VERSION})
