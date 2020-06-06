0543cb02f372bc1e07bf223d8d80afae7390049e
Matthew Paletta <mattpaletta@gmail.com>
2020-06-06 15:03:12 -0700

    Got engine building on windows

--- external/assimp.cmake
+++ external/assimp.cmake
@@ -1,8 +1,8 @@
 set(ASSIMP_VERSION v5.0.0)
 # set(BUILD_SHARED_LIBS ON)
-set(ASSIMP_BUILD_ASSIMP_TOOLS OFF)
-set(ASSIMP_BUILD_BUILD_SAMPLES OFF)
-set(ASSIMP_BUILD_TESTS OFF)
-set(ASSIMP_INSTALL ON)
+option(ASSIMP_BUILD_ASSIMP_TOOLS "Assimp Tools" OFF)
+option(ASSIMP_BUILD_BUILD_SAMPLES "Assimp samples" OFF)
+option(ASSIMP_BUILD_TESTS "Assimp tests" OFF)
+option(ASSIMP_INSTALL "Assimp install" ON)
 
 fetch_extern(assimp https://github.com/assimp/assimp.git ${ASSIMP_VERSION})
--- external/fiber_tasking_lib.cmake
+++ external/fiber_tasking_lib.cmake
@@ -1,8 +1,8 @@
 # You can use the FTL_BUILD_TESTS variable to enable / disable test building
-set( FTL_BUILD_TESTS OFF)
+option( FTL_BUILD_TESTS "FTL Test" OFF)
 
 # You can use the FTL_BUILD_BENCHMARKS variable to enable / disable benchamark building
-set( FTL_BUILD_BENCHMARKS OFF)
+option( FTL_BUILD_BENCHMARKS "FTL benchmarks" OFF)
 
 set(FTL_VERSION 7a4a642e54f307499c8d2da84d4a93b977835406)
 
